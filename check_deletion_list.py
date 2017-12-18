0ff08df826aa6f277dc25518f4b41fcb2c264743
Rachael Creager <rccreager@gmail.com>
2017-12-18 16:51:40 -0500

    more improvements to check_deletion_list

--- check_deletion_list.py
+++ check_deletion_list.py
@@ -1,5 +1,6 @@
 import urllib
 import re
+import smtplib
 
 ftags = ("FTAG1", "FTAG2", "FTAG3", "FTAG4" )
 
@@ -11,9 +12,13 @@ for ftag in ftags:
     if len(lines) == 4:
         print "Nothing to be deleted for " + ftag
     else:
+        print "Deletion list for " + ftag
         for line in lines:
-            if (line != "<tr>\n") and (line != "</tr>\n") and (line != "</table><br>\n"):
-                print line    
+            if re.match("(.*mc.*)|(.*data.*)",line):
+                if not re.match("Sort by.*",line):
+                    print line.replace("<td>","").replace("</td>","").replace("\n","")
+            #if (line != "<tr>\n") and (line != "</tr>\n") and (line != "</table><br>\n"):
+            #    print line    
 
     #for i,v in enumerate(lines):
     #    if re.match(".*FTAG.*",lines[i]):
