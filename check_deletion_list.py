d84af6e857a5c0bd7964fdbd07dca18e4f7c0ed8
Rachael Creager <rccreager@gmail.com>
2017-12-18 16:28:42 -0500

    added some log files, improved check_deletion_list

--- check_deletion_list.py
+++ check_deletion_list.py
@@ -1,6 +1,23 @@
 import urllib
+import re
 
-link = "http://adc-mon.cern.ch/DAODProduction//DAILYCHECK_MC"
-f = urllib.urlopen(link)
-myfile = f.read()
-print myfile
+ftags = ("FTAG1", "FTAG2", "FTAG3", "FTAG4" )
+
+for ftag in ftags:
+    link = "http://adc-mon.cern.ch/DAODProduction/DAILYCHECK_MC/derivs/obs_tbl_DAOD_"+ftag+".html"
+    f = urllib.urlopen(link)
+    lines = f.readlines()
+    
+    if len(lines) == 4:
+        print "Nothing to be deleted for " + ftag
+    else:
+        for line in lines:
+            if (line != "<tr>\n") and (line != "</tr>\n") and (line != "</table><br>\n"):
+                print line    
+
+    #for i,v in enumerate(lines):
+    #    if re.match(".*FTAG.*",lines[i]):
+    #        print lines[i]
+    #        print lines[i+1]
+    #        print lines[i+2]
+
--- plot_tools/driver.sh
+++ plot_tools/driver.sh
@@ -3,17 +3,12 @@ WPs=("MV2c10at60tagged" "MV2c10at70tagged" "MV2c10at77tagged" "MV2c10at85tagged"
 tags=("tag")
 #tags=("tag" "untag" )
 datasets=("01758BT_with_HF_corr_normalization_fix" )
-#datasets=("ingo_fitter" "ingo_templates_no_overflow" "ingo_templates_with_overflow" "old_tuples_old_BT" "new_tuples_old_BT" "new_tuples_new_BT"  )
-#WPs=("MV2c10at85tagged")
-#tags=("tag")
-#datasets=("old_tuples_old_BT")
-functionName="print_all_bins_integral"
-#functionName="print_summary"
+functionName="print_error_summary"
 #hists=("bFrac_untagStart" )
 #hists=("NTagB" "NUntagB")
-hists=("pTrel_data_tag" "pTrel_data_untag")
-#hists=("measuredBEfficiencies" "trueBEfficiencies")
-#hists=( "b-Fraction_tag" "b-Fraction_untag" )
+#hists=("pTrel_data_tag" "pTrel_data_untag")
+hists=("measuredBEfficiencies" "trueBEfficiencies")
+#hists=( "b-FractionErr_tag" "b-FractionErr_untag" )
 
 for hist in ${hists[@]}
 do
@@ -23,8 +18,8 @@ do
         do
             for WP in ${WPs[@]}
             do
-                #newMacroName=${functionName}_${hist}_${WP}_${tag}_${dataset}.C
-                newMacroName=${functionName}_${WP}_${tag}_${dataset}.C
+                newMacroName=${functionName}_${hist}_${WP}_${tag}_${dataset}.C
+                #newMacroName=${functionName}_${WP}_${tag}_${dataset}.C
                 cp ${functionName}.C $newMacroName
                 sed -i .backup "s#WP#$WP#g" $newMacroName
                 sed -i .backup "s#TAG#$tag#g" $newMacroName
